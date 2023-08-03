28bb06a43bb495cf0789a8028a8ded264c2b37e3
pr-maryanarty <maryana.dementeva@gmail.com>
2023-08-03 23:47:36 +0100

    task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n >= 0) {
+        return n.toString().split("").reverse().join("");
+    } else {
+        let number = n * -1;
+        return number.toString().split("").reverse().join("");
+    }
+};
